f04b24c60157b7e71fa5466cd1b9db09bdc4b850
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-11-28 23:52:06 +0545

    External JS added

---
+++ day 01/first.js
@@ -0,0 +1,10 @@
+// This is my first seperate js file over here
+//single line comments in js
+/*
+only comments 
+    doesn't looks
+            like
+            java in javascript
+            * Most of is similar
+*/
+console.log("another line on console from seperate js file")
