8a96f4ab572b874a51b258ed21abdbed9cfaa543
SatyaRajAwasth1 <awasthisatyaraj2060gmail.com>
2022-11-29 22:30:42 +0545

    day 01 code files

--- day 01/first.js
+++ day 01/first.js
@@ -1,4 +1,5 @@
 // This is my first seperate js file over here
+// ****** External Script **********
 //single line comments in js
 /*
 only comments 
@@ -12,3 +13,8 @@ console.log("another line on console from seperate js file")
 console.log('Learners: ',1,"Satya",2,'Janak',3,'Abhisekh',"4",'Srawan',5,"Bishal -> Coming on from day 2");
 // semi colons are optional in js
 // single quote or double quote can be used for strings.
+
+//performing arithmetics inside console
+console.log(2+4)
+console.log("remainder while dividing 5 by 2 = ",5%2)
+//all the arithmetics in js are similar to c.
