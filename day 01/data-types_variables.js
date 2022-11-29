b42968e8b49f51003709705ce3064027e1b01da0
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-11-29 22:30:42 +0545

    day 01 code files

---
+++ day 01/data-types_variables.js
@@ -0,0 +1,32 @@
+// All about Data types here
+// js is dynamically typed language.
+
+let name = "Satya Raj Awasthi"; 
+
+var age = 20;
+const pi = 3.14; //constants doens't get changed.
+// let can be used to make any type of variable
+//semi colon are not mandatory
+
+/*
+Diff betn var and let
+--> scope of let is inside block only
+--> but scope of var is also outside the block.    
+*/
+
+document.write("<br>got it Name is: ",name, " <br> age = "+age);
+
+
+++age;
+
+document.writeln("<br> age = "+age);
+console.log(typeof true) // boolean 
+
+console.log(typeof "js isn't favourite")
+console.log(typeof 3.54534656478568734625)
+console.log(typeof null)
+
+
+
+
+
