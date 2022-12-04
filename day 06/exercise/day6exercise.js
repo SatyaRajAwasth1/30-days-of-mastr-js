76232b848669564c9119975d288e39de39b762e4
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-12-04 23:59:07 +0545

    # pattern done

--- day 06/exercise/day6exercise.js
+++ day 06/exercise/day6exercise.js
@@ -1,1 +1,23 @@
 // exercise of day 6 
+// about loops
+// level 1
+
+//q.n.4
+
+/*
+pattern
+#
+##
+###
+####
+#####
+######
+#######
+*/
+let n = 5;
+for(let i = 0; i<n; i++){
+    for(let j = 0; j<i; j++){
+    console.log("#");
+}
+console.log(" ")
+}
