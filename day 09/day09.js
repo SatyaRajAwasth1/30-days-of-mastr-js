e46a19609f3ed9ada16ac259731b139e9ba30ff8
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-12-08 22:26:30 +0545

    RETURNING FUNCTION

--- day 09/day09.js
+++ day 09/day09.js
@@ -1,7 +1,8 @@
 /* Higher order functions in js
     -> Functions that take functions as parameter or return a function as value.
 */
-// a function to square number
+/*
+ a function to square number
 const square = (n) => {
 return n**2; //returns the square of n
 }
@@ -13,3 +14,17 @@ function displaySqr(square, n){
 }
 let n = prompt("Enter a number to square");
 console.log(`square of ${n} = ${displaySqr(square , n)}`);
+*/
+
+//Returning a function
+function myName(firstName){
+    middleName = (mn = " ") =>{
+    lastName = (ln) => {
+        return firstName + mn + ln ;
+    }
+        return lastName;
+    }
+        return middleName;
+}
+console.log("My name: "+myName("Satya ")("Raj ")("Awasthi"));
+console.log("name: "+myName("Puran ")()(" Bhatta"));
