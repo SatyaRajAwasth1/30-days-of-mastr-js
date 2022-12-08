2afdda840f582a6a9bac8fb6bf63b8a5b3dd5aaf
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-12-08 22:05:01 +0545

    function as parameter

--- day 09/day09.js
+++ day 09/day09.js
@@ -1,3 +1,15 @@
 /* Higher order functions in js
     -> Functions that take functions as parameter or return a function as value.
 */
+// a function to square number
+const square = (n) => {
+return n**2; //returns the square of n
+}
+// higher order function by calling a function as parameter
+
+function displaySqr(square, n){
+    let sqr = square(n);
+    return sqr;
+}
+let n = prompt("Enter a number to square");
+console.log(`square of ${n} = ${displaySqr(square , n)}`);
