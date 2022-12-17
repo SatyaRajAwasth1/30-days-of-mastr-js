b5d574a9bc628c09a4ecdbaace678108a847e9fb
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-12-17 22:55:30 +0545

    .

---
+++ day 21/dom.js
@@ -0,0 +1,9 @@
+/*
+DOM: Document object Model
+HTML elements can be selected from js using tag name, id, class name or other attributes like from css
+*/
+
+// getting element by tag name
+// getting all the h2 tags in a variable as array
+/* let h2s = document.getElementsByTagName(h2);
+prompt('h2s') */
